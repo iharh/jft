aa5e05c220c0d8e59afeabc6de206830b3c5ba08
Ihar Hancharenka <iigoncharenko@gmail.com>
2018-06-20 11:36:31 +0300

    m

--- src/main/cpp/clb/test.cpp
+++ src/main/cpp/clb/test.cpp
@@ -17,6 +17,33 @@ main(void)
     int *pI = new int;
     *pI = 7;
 
+    fta.getLabels();
+
+    fta.getNWords();
+    fta.getDim();
+    fta.getContextWindowSize();
+    fta.getEpoch();
+    fta.getNSampledNegatives();
+    fta.getWordNgrams();
+    fta.getLossName();
+    fta.getModelName();
+    fta.getBucket();
+    fta.getLabelPrefix();
+
+    /*
+    HIGH_EFFORT(-5.0),
+    SOME_EFFORT(-3.0),
+    NEUTRAL(0.0),
+    SOME_EASE(3.0),
+    HIGH_EASE(5.0);
+    */
+    int32_t effortLevelValuesLen = 5;
+    const std::string text("I like cellphone very much");
+
+    fta.predictProba(text, effortLevelValuesLen);
+
+    fta.unloadModel();
+
     std::wcout << L"done: " << *pI << std::endl;
 
     return 0;
